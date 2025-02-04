33dd48d878c92002a7c923827600b1c143256e8c
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-02-04 15:53:19 +0200

    Update opilane_localhost_server.sql
    04.02.2025 GOOD WORK

--- opilane_localhost_server.sql
+++ opilane_localhost_server.sql
@@ -34,4 +34,62 @@ foreign key (Language) references Language(id)
 );
 insert into keelevalik(valikunimetus, opilaneid, Language)
 Values ('valik B', 1, 2)
+---------------------------------------------------------------------------------Protseduur
+	
+---1. protseduur, mis lisab kasutaja koos kogu teabega
+create procedure lisaopilane
+@lnimi varchar(25),
+@lperen varchar(25),
+@sunn date,
+@stip bit,
+@adr text,
+@kh decimal(2,1)
+as
+begin
+insert into opilane(eesnimi, perenimi, sunniaeg, stip, aadress, keskmine_hinne)
+values (@lnimi, @lperen, @sunn, @stip, @adr, @kh)
 
+exec lisaopilane @lnimi='idk', @lperen='qwe', @sunn='2007-11-13', @stip=1, @adr='addresss here', @kh=4.6;
+
+end;
+
+---2. protseduur, mis kustutab opilane id järgi
+create procedure kustutaopilane
+@opilaneId int
+as
+begin
+select * from linn;
+delete from opilane where opilaneid=@opilaneId
+select * from opilane;
+end;
+
+---kutse
+exec kustutaopilane 6;
+
+---3. Protseduur, mis otsib opilane esimese tähte järgi
+create procedure opilaneOtsing
+@taht char(1)
+as
+begin
+select * from opilane
+where eesnimi like @taht + '%';
+end;
+---kutse
+exec opilaneOtsing M;
+
+---4. andmete uuendamine, stip muudetakse
+update opilane set stip=stip*1.1
+select * from opilane
+update opilane set stip=stip*1.1
+where opilaneId=7;
+
+create procedure stipuuendus
+@opilaneid int,
+@koef decimal(2,1)
+as
+begin
+update opilane set stip=stip*@koef
+where opilaneId=@opilaneId;
+select * from opilane;
+end;
+exec stipuuendus 7, 0;
